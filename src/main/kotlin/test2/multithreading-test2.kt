70608c0b664c61e83e0b66f4b8029658a70dbe4a
rRain <rrainpath@gmail.com>
2022-05-28 18:20:38 +0800

    some additions

---
+++ src/main/kotlin/test2/multithreading-test2.kt
@@ -0,0 +1,41 @@
+package test2
+
+import java.util.*
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
+import java.util.concurrent.atomic.AtomicInteger
+
+private var size = 1000
+
+private val list1: MutableList<Int> = Collections.synchronizedList(ArrayList())
+private val list2: MutableList<Int> = Collections.synchronizedList(ArrayList())
+
+private val counter: AtomicInteger = AtomicInteger()
+private val resultList: MutableList<Int> = Collections.synchronizedList(ArrayList())
+
+fun main(){
+
+    repeat(size) {
+        list1.add(Random().nextInt())
+        list2.add(Random().nextInt())
+        resultList.add(0)
+    }
+
+    val threadPool = Executors.newFixedThreadPool(3)
+
+    repeat(size) { threadPool.submit(::sum) }
+
+    threadPool.shutdown()
+    threadPool.awaitTermination(Long.MAX_VALUE, TimeUnit.MILLISECONDS)
+
+    println(list1)
+    println(list2)
+    println(resultList)
+}
+
+
+private fun sum(){
+    val i = counter.getAndIncrement()
+    if (i<0) return
+    resultList[i] = list1[i] + list2[i]
+}
